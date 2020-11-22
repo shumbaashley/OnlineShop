70d8177e4e985cf84d4ebe7ee0bbffef545398a8
shumbaashley <ashleytshumba@gmail.com>
2020-11-23 00:10:35 +0200

    product details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,25 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from 'react-bootstrap';
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 const App = () => {
   return (
-      <>
+    <Router>
       <Header/>
+      <Switch>
         <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route exact path="/cart" component={HomeScreen}/>
+          <Route exact path="/login" component={HomeScreen}/>
+          <Route exact path="/product/:id" component={ProductScreen}/>
         </Container>
         </main>
+        </Switch>
       <Footer/>
-      </>    
+      </Router> 
   );
 }
 
